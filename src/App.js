f70c4304c67255806d3a0a6881372573758cb6cb
Sakshi Gupta <sakshgup@adobe.com>
2019-03-25 15:07:59 +0530

    Adding routes for modules

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <header className="App-header">
+              <p className="App-name">Adobe Excperience Cloud</p>
+              <Link to="/module1" className="App-route">Module 1</Link>
+              <Link to="/module2" className="App-route">Module 2</Link>
+          </header>
+          <Route exact path="/module1" component={Home} />
+          <Route path="/module2" component={About} />
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
